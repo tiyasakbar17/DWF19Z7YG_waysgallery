9f07197c650cfd314ce45be6e219d9574c1c7a03
Muhammad Tiyas Fachreza Akbar <72636589+tiyasakbar17@users.noreply.github.com>
2021-06-17 07:45:01 +0700

    Update Auth.jsx

--- src/redux/actions/Auth.jsx
+++ src/redux/actions/Auth.jsx
@@ -18,7 +18,7 @@ const configForm = (dispatch) => ({
         dispatch(showProgress(percentage));
     },
 });
-const baseUrl = 'https://finaltask-tiyas.herokuapp.com/api/v1'
+const baseUrl = 'https://tiyas-ways-gallery.herokuapp.com/api/v1'
 
 export const loadData = () => async dispatch => {
 
